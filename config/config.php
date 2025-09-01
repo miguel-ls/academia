b383fce3ccec718b9f9dba424d3d56466bcb8373
Miguel Lopez <miguel.lopez@codesicorp.com>
2025-09-01 18:54:42 -0500

    db

--- config/config.php
+++ config/config.php
@@ -6,8 +6,8 @@
 
 // Configuración de la Base de Datos
 define('DB_HOST', '127.0.0.1'); // O la IP de tu servidor de BD
-define('DB_USER', 'root');      // Usuario de la BD
-define('DB_PASS', 'password');  // Contraseña de la BD (cámbiala por una segura)
+define('DB_USER', 'miguel');
+define('DB_PASS', 'Miguel123!'); // Colocar la contraseña si es necesaria
 define('DB_NAME', 'academia_cursos'); // Nombre de la BD
 
 // Configuración del Sitio
@@ -15,11 +15,11 @@ define('SITE_URL', 'http://localhost/academia'); // URL base de tu sitio
 define('SITE_NAME', 'Academia de Cursos');
 
 // Configuración para el envío de correos (para 2FA y notificaciones)
-define('SMTP_HOST', 'smtp.example.com');
-define('SMTP_USER', 'user@example.com');
-define('SMTP_PASS', 'your_smtp_password');
-define('SMTP_PORT', 587); // O 465 para SSL
-define('SMTP_FROM_EMAIL', 'no-reply@example.com');
+define('MAIL_HOST', 'mail.codesicorp.com');
+define('MAIL_USER', 'ventas.sistemas@codesicorp.com');
+define('MAIL_PASS', 'Mcausit@.977611');
+define('MAIL_PORT', 465); // o 465
+define('SMTP_FROM_EMAIL', 'ventas.sistemas@codesicorp.com');
 define('SMTP_FROM_NAME', 'Academia de Cursos');
 
 // Otras configuraciones
